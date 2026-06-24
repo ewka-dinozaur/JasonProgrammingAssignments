f61273950bf7cb1f68e75efa604ca71e0c290d46
Ewa Laskowska <ewa.maria.laskowska@gmail.com>
2026-06-24 14:51:08 +1200

    Calculator project added.

---
+++ Kalkulator/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using Kalkulator;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Kalkulator
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+        public double Mul(double a, double b)
+        {
+            return a * b;
+        }
+        public double Div(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Nie można dzielić przez zero.");
+            return a / b;
+        }
+    }
+
+
+}
---
+++ Kalkulator/Program.cs
@@ -0,0 +1,48 @@
+﻿using Kalkulator;
+
+Console.WriteLine("Hello, I am your calculator");
+
+Calculator calc = new Calculator();
+
+Console.Write("Podaj pierwszą liczbę: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Podaj drugą liczbę: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Wybierz operację: +, -, *, /");
+string op = Console.ReadLine();
+
+double result = 0;
+
+switch (op)
+{
+    case "+":
+        result = calc.Add(a, b);
+        break;
+    case "-":
+        result = calc.Sub(a, b);
+        break;
+    case "*":
+        result = calc.Mul(a, b);
+        break;
+    case "/":
+        {
+            try
+            {
+                result = calc.Div(a, b);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Błąd: " + ex.Message);
+            }
+        }
+        break;
+
+
+    default:
+        Console.WriteLine("Nieznany operator.");
+        break;
+}
+
+Console.WriteLine($"Wynik: {result}");
